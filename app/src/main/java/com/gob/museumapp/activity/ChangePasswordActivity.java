989e0545fd6ca4f37bbf8a7b14dfdd6d62cc7455
RicardoMLu <imlu18@outlook.com>
2022-01-12 20:23:01 +0800

    修复了一些bug

--- app/src/main/java/com/gob/museumapp/activity/ChangePasswordActivity.java
+++ app/src/main/java/com/gob/museumapp/activity/ChangePasswordActivity.java
@@ -2,6 +2,7 @@ package com.gob.museumapp.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Looper;
 import android.view.View;
@@ -13,21 +14,27 @@ import android.widget.Toast;
 import com.gob.museumapp.R;
 import com.gob.museumapp.db.DBHelper;
 
+import java.util.List;
+import java.util.Map;
+
 public class ChangePasswordActivity extends Activity implements View.OnClickListener
 {
-    private EditText phoneEdit=null;
-    private EditText passwordEdit=null;
+    private EditText oldPwd=null;
+    private EditText newPwd=null;
     private Button confirm=null;
+    private int user_id = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_change_password);
-        phoneEdit = findViewById(R.id.phone);
-        passwordEdit = findViewById(R.id.password);
+        oldPwd = findViewById(R.id.old_pwd);
+        newPwd = findViewById(R.id.new_pwd);
         confirm = findViewById(R.id.change_confirm);
         confirm.setOnClickListener(this);
+        SharedPreferences pref=getSharedPreferences("user_data",MODE_PRIVATE);
+        user_id = pref.getInt("id", 1);
     }
 
     @Override
@@ -42,15 +49,31 @@ public class ChangePasswordActivity extends Activity implements View.OnClickList
                     public void run()
                     {
                         DBHelper dbhelper=new DBHelper();
-                        String phone=phoneEdit.getText().toString();
-                        String password=passwordEdit.getText().toString();
-                        String query="UPDATE User SET user_password='"+password+"' WHERE user_phone='"+phone+"'";
+                        String oldValue=oldPwd.getText().toString();
+                        String newValue=newPwd.getText().toString();
+                        String query="select * from User where user_id = " + user_id + " and user_password = '" + oldValue + "'";
                         dbhelper.setSql(query);
-                        dbhelper.executeUpdate();
-                        Looper.prepare();
-                        Toast.makeText(ChangePasswordActivity.this,"Change successfully", Toast.LENGTH_LONG).show();
-                        Intent intent = new Intent(ChangePasswordActivity.this, LoginActivity.class);
-                        startActivity(intent);
+                        List<Map<String, Object>> rs = dbhelper.executeQuery();
+                        if(rs.size() > 0){
+                            DBHelper helper1 = new DBHelper();
+                            String sql1 = "update User set user_password = '" + newValue + "' where user_id = " + user_id;
+                            helper1.setSql(sql1);
+                            int r = helper1.executeUpdate();
+                            Looper.prepare();
+                            if(r == 1){
+                                Toast.makeText(ChangePasswordActivity.this,"Change successfully", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(ChangePasswordActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }
+                            else{
+                                Toast.makeText(ChangePasswordActivity.this,"Change Failed. Please retry later", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        else{
+                            Looper.prepare();
+                            Toast.makeText(ChangePasswordActivity.this,"Wrong old password", Toast.LENGTH_LONG).show();
+                            oldPwd.setText("");
+                        }
                     }
                 }).start();
 
